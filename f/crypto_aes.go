d1a61d38f70050cecde9ba781f6073aaaaa4db50
Angenal <angenal@hotmail.com>
2020-03-10 09:27:11 +0800

    Update crypto_aes.go

--- f/crypto_aes.go
+++ f/crypto_aes.go
@@ -5,7 +5,7 @@ import (
 	"crypto/cipher"
 )
 
-// CryptoAesCBCEncrypt aes CBC模式+key(16bytes)+iv(16bytes).
+// CryptoAesCBCEncrypt aes CBC模式+key(16/24/32bytes)+iv(16/24/32bytes).
 // encryptedString := hex.EncodeToString(encryptedBytes)
 // encryptedString := base64.StdEncoding.EncodeToString(encryptedBytes)
 func CryptoAesCBCEncrypt(origData, key, iv []byte) ([]byte, error) {
@@ -18,19 +18,19 @@ func CryptoAesCBCEncrypt(origData, key, iv []byte) ([]byte, error) {
 	return encrypted, nil
 }
 
-// CryptoAesCBCEncryptWithHmacSHA1 aes CBC模式+key(16bytes)+salt(8bytes)+iv(16bytes), iterations 1000, output 32 bytes.
+// CryptoAesCBCEncryptWithHmacSHA1 aes CBC模式+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes), iterations 1000, output 32 bytes.
 func CryptoAesCBCEncryptWithHmacSHA1(origData, key, salt, iv []byte, iterations, outLen int) ([]byte, error) {
 	password := CryptoSecretKeyPBKDF2WithHmacSHA1(key, salt, iterations, outLen)
 	return CryptoAesCBCEncrypt(origData, password[0:32], iv)
 }
 
-// CryptoAesCBCEncryptWithHmacSHA256 aes CBC模式+key(16bytes)+salt(8bytes)+iv(16bytes), iterations 1000, output 32 bytes.
+// CryptoAesCBCEncryptWithHmacSHA256 aes CBC模式+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes), iterations 1000, output 32 bytes.
 func CryptoAesCBCEncryptWithHmacSHA256(origData, key, salt, iv []byte, iterations, outLen int) ([]byte, error) {
 	password := CryptoSecretKeyPBKDF2WithHmacSHA256(key, salt, iterations, outLen)
 	return CryptoAesCBCEncrypt(origData, password[0:32], iv)
 }
 
-// CryptoAesCBCDecrypt aes CBC模式+key(16bytes)+iv(16bytes).
+// CryptoAesCBCDecrypt aes CBC模式+key(16/24/32bytes)+iv(16/24/32bytes).
 // encryptedBytes, err := hex.DecodeString(encryptedString)
 // encryptedBytes, err := base64.StdEncoding.DecodeString(encryptedString)
 func CryptoAesCBCDecrypt(encrypted, key, iv []byte) ([]byte, error) {
@@ -43,19 +43,19 @@ func CryptoAesCBCDecrypt(encrypted, key, iv []byte) ([]byte, error) {
 	return origData, nil
 }
 
-// CryptoAesCBCDecryptWithHmacSHA1 aes CBC模式+key(16bytes)+salt(8bytes)+iv(16bytes), iterations 1000, output 32 bytes.
+// CryptoAesCBCDecryptWithHmacSHA1 aes CBC模式+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes), iterations 1000, output 32 bytes.
 func CryptoAesCBCDecryptWithHmacSHA1(encrypted, key, salt, iv []byte, iterations, outLen int) ([]byte, error) {
 	password := CryptoSecretKeyPBKDF2WithHmacSHA1(key, salt, iterations, outLen)
 	return CryptoAesCBCDecrypt(encrypted, password[0:32], iv)
 }
 
-// CryptoAesCBCDecryptWithHmacSHA256 aes CBC模式+key(16bytes)+salt(8bytes)+iv(16bytes), iterations 1000, output 32 bytes.
+// CryptoAesCBCDecryptWithHmacSHA256 aes CBC模式+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes), iterations 1000, output 32 bytes.
 func CryptoAesCBCDecryptWithHmacSHA256(encrypted, key, salt, iv []byte, iterations, outLen int) ([]byte, error) {
 	password := CryptoSecretKeyPBKDF2WithHmacSHA256(key, salt, iterations, outLen)
 	return CryptoAesCBCDecrypt(encrypted, password[0:32], iv)
 }
 
-// CryptoAesBlockMode New Aes BlockMode Method.
+// CryptoAesBlockMode New Aes BlockMode Method+key(16/24/32bytes)+iv(16/24/32bytes).
 // blockMode, blockSize := CryptoAesBlockMode(key, iv, cipher.NewCBCEncrypter)
 // blockMode, blockSize := CryptoAesBlockMode(key, iv, cipher.NewCBCDecrypter)
 func CryptoAesBlockMode(key, iv []byte, f func(cipher.Block, []byte) cipher.BlockMode) (cipher.BlockMode, int) {
@@ -65,7 +65,7 @@ func CryptoAesBlockMode(key, iv []byte, f func(cipher.Block, []byte) cipher.Bloc
 	return f(block, iv[:blockSize]), blockSize
 }
 
-// CryptoAesStream New Aes Stream Method.
+// CryptoAesStream New Aes Stream Method+key(16/24/32bytes)+iv(16/24/32bytes).
 // stream := CryptoAesStream(key, iv, cipher.NewCFBDecrypter)
 // stream := CryptoAesStream(key, iv, cipher.NewCFBEncrypter)
 func CryptoAesStream(key, iv []byte, f func(cipher.Block, []byte) cipher.Stream) cipher.Stream {
