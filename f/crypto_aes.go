26c467ab3429e89ea24a90a0b6a77d7fd0fe8364
Angenal <angenal@hotmail.com>
2020-03-11 09:16:00 +0800

    Update crypto_aes.go

--- f/crypto_aes.go
+++ f/crypto_aes.go
@@ -5,7 +5,7 @@ import (
 	"crypto/cipher"
 )
 
-// CryptoAesCBCEncrypt aes CBC模式+key(16/24/32bytes)+iv(16/24/32bytes).
+// CryptoAesCBCEncrypt AES/CBC/PKCS5Padding+key(16/24/32bytes)+iv(16/24/32bytes).
 // encryptedString := hex.EncodeToString(encryptedBytes)
 // encryptedString := base64.StdEncoding.EncodeToString(encryptedBytes)
 func CryptoAesCBCEncrypt(origData, key, iv []byte) []byte {
@@ -18,19 +18,19 @@ func CryptoAesCBCEncrypt(origData, key, iv []byte) []byte {
 	return encrypted
 }
 
-// CryptoAesCBCEncryptWithHmacSHA1 aes CBC模式+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes)+iterations(1000)+outLen(32).
+// CryptoAesCBCEncryptWithHmacSHA1 AES/CBC/PKCS5Padding+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes)+iterations(1000)+outLen(32).
 func CryptoAesCBCEncryptWithHmacSHA1(origData, key, salt, iv []byte, iterations, outLen int) []byte {
 	password := CryptoSecretKeyPBKDF2WithHmacSHA1(key, salt, iterations, outLen)
 	return CryptoAesCBCEncrypt(origData, password[0:32], iv)
 }
 
-// CryptoAesCBCEncryptWithHmacSHA256 aes CBC模式+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes)+iterations(1000)+outLen(32).
+// CryptoAesCBCEncryptWithHmacSHA256 AES/CBC/PKCS5Padding+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes)+iterations(1000)+outLen(32).
 func CryptoAesCBCEncryptWithHmacSHA256(origData, key, salt, iv []byte, iterations, outLen int) []byte {
 	password := CryptoSecretKeyPBKDF2WithHmacSHA256(key, salt, iterations, outLen)
 	return CryptoAesCBCEncrypt(origData, password[0:32], iv)
 }
 
-// CryptoAesCBCDecrypt aes CBC模式+key(16/24/32bytes)+iv(16/24/32bytes).
+// CryptoAesCBCDecrypt aes AES/CBC/PKCS5Padding+key(16/24/32bytes)+iv(16/24/32bytes).
 // encryptedBytes, err := hex.DecodeString(encryptedString)
 // encryptedBytes, err := base64.StdEncoding.DecodeString(encryptedString)
 func CryptoAesCBCDecrypt(encrypted, key, iv []byte) []byte {
@@ -42,13 +42,13 @@ func CryptoAesCBCDecrypt(encrypted, key, iv []byte) []byte {
 	return CryptoPKCS5UnPadding(origData)
 }
 
-// CryptoAesCBCDecryptWithHmacSHA1 aes CBC模式+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes)+iterations(1000)+outLen(32).
+// CryptoAesCBCDecryptWithHmacSHA1 AES/CBC/PKCS5Padding+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes)+iterations(1000)+outLen(32).
 func CryptoAesCBCDecryptWithHmacSHA1(encrypted, key, salt, iv []byte, iterations, outLen int) []byte {
 	password := CryptoSecretKeyPBKDF2WithHmacSHA1(key, salt, iterations, outLen)
 	return CryptoAesCBCDecrypt(encrypted, password[0:32], iv)
 }
 
-// CryptoAesCBCDecryptWithHmacSHA256 aes CBC模式+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes)+iterations(1000)+outLen(32).
+// CryptoAesCBCDecryptWithHmacSHA256 AES/CBC/PKCS5Padding+key(16/24/32bytes)+salt(8bytes)+iv(16/24/32bytes)+iterations(1000)+outLen(32).
 func CryptoAesCBCDecryptWithHmacSHA256(encrypted, key, salt, iv []byte, iterations, outLen int) []byte {
 	password := CryptoSecretKeyPBKDF2WithHmacSHA256(key, salt, iterations, outLen)
 	return CryptoAesCBCDecrypt(encrypted, password[0:32], iv)
