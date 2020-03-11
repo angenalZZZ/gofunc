d6dd6d028cdc91211ebf3a10a7a77c3180638453
Angenal <angenal@hotmail.com>
2020-03-11 09:53:29 +0800

    Update crypto_des.go

--- f/crypto_des.go
+++ f/crypto_des.go
@@ -14,9 +14,9 @@ func CryptoDesCBCEncrypt(origData, key, iv []byte) []byte {
 	MustBytes(iv, 16)
 	block, err := des.NewCipher(key)
 	Must(err)
-	blockSize := block.BlockSize()
-	origData = CryptoPKCS5Padding(origData, blockSize)
-	blockMode := cipher.NewCBCEncrypter(block, iv[:blockSize])
+	bs := block.BlockSize()
+	origData = CryptoPKCS5Padding(origData, bs)
+	blockMode := cipher.NewCBCEncrypter(block, iv[:bs])
 	encrypted := make([]byte, len(origData))
 	blockMode.CryptBlocks(encrypted, origData)
 	return encrypted
@@ -31,8 +31,8 @@ func CryptoDesCBCDecrypt(encrypted, key, iv []byte) []byte {
 	MustBytes(iv, 16)
 	block, err := des.NewCipher(key)
 	Must(err)
-	blockSize := block.BlockSize()
-	mode := cipher.NewCBCDecrypter(block, iv[:blockSize])
+	bs := block.BlockSize()
+	mode := cipher.NewCBCDecrypter(block, iv[:bs])
 	origData := make([]byte, len(encrypted))
 	mode.CryptBlocks(origData, encrypted)
 	return CryptoPKCS5UnPadding(origData)
@@ -43,17 +43,17 @@ func CryptoDesECBEncrypt(origData, key []byte) []byte {
 	MustBytes(key, 8)
 	block, err := des.NewCipher(key)
 	Must(err)
-	blockSize := block.BlockSize()
-	origData = CryptoPKCS5Padding(origData, blockSize)
-	if len(origData)%blockSize != 0 {
+	bs := block.BlockSize()
+	origData = CryptoPKCS5Padding(origData, bs)
+	if len(origData)%bs != 0 {
 		return nil
 	}
 	out := make([]byte, len(origData))
 	dst := out
 	for len(origData) > 0 {
-		block.Encrypt(dst, origData[:blockSize])
-		origData = origData[blockSize:]
-		dst = dst[blockSize:]
+		block.Encrypt(dst, origData[:bs])
+		origData = origData[bs:]
+		dst = dst[bs:]
 	}
 	return out
 }
@@ -63,16 +63,16 @@ func CryptoDesECBDecrypt(encrypted, key []byte) []byte {
 	MustBytes(key, 8)
 	block, err := des.NewCipher(key)
 	Must(err)
-	blockSize := block.BlockSize()
-	if len(encrypted)%blockSize != 0 {
+	bs := block.BlockSize()
+	if len(encrypted)%bs != 0 {
 		return nil
 	}
 	out := make([]byte, len(encrypted))
 	dst := out
 	for len(encrypted) > 0 {
-		block.Decrypt(dst, encrypted[:blockSize])
-		encrypted = encrypted[blockSize:]
-		dst = dst[blockSize:]
+		block.Decrypt(dst, encrypted[:bs])
+		encrypted = encrypted[bs:]
+		dst = dst[bs:]
 	}
 	return CryptoPKCS5UnPadding(out)
 }
@@ -87,8 +87,8 @@ func CryptoDesECBTripleEncrypt(origData, key []byte) []byte {
 	k3 := tk[16:]
 	block, err := des.NewCipher(k1)
 	Must(err)
-	blockSize := block.BlockSize()
-	origData = CryptoPKCS5Padding(origData, blockSize)
+	bs := block.BlockSize()
+	origData = CryptoPKCS5Padding(origData, bs)
 	buf1 := cryptoDesECBEncrypt(origData, k1)
 	buf2 := cryptoDesECBDecrypt(buf1, k2)
 	buf3 := cryptoDesECBEncrypt(buf2, k3)
