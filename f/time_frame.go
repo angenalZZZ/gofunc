aa2655322c96aed0b22cf213657dfe6c834c4440
Angenal <angenal@hotmail.com>
2020-06-11 08:57:10 +0800

    Update time_frame.go

--- f/time_frame.go
+++ f/time_frame.go
@@ -23,16 +23,12 @@ func NewTimeFrames(since, until time.Time, duration time.Duration) []*TimeFrame 
 	since, until = time.Unix(since.Unix(), 0).Local(), time.Unix(until.Unix(), 0).Local()
 	a, s := since, make([]*TimeFrame, 0)
 	for a.Before(until) {
-		// set to date = from + duration
 		t := a.Add(duration)
-
 		if t.Before(until) {
 			s = append(s, NewTimeFrame(a, t))
 		} else {
 			s = append(s, NewTimeFrame(a, until))
 		}
-
-		// increment from date with 1
 		a = t
 	}
 	return s
@@ -40,7 +36,7 @@ func NewTimeFrames(since, until time.Time, duration time.Duration) []*TimeFrame 
 
 func (t *TimeFrame) In(t2 time.Time) bool {
 	u := t2.Unix()
-	return t.Since.UnixSecondTimeStamp <= u && u < t.Until.UnixSecondTimeStamp // Excluding Until Time
+	return t.Since.UnixSecondTimeStamp <= u && u < t.Until.UnixSecondTimeStamp
 }
 
 func (t *TimeFrame) String() string {
