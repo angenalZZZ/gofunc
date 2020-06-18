b0931e4edbe51ff02c51456736ee2f1b5c3b4c88
yangzhou <angenal@hotmail.com>
2020-06-18 20:29:21 +0800

    http restful client request

---
+++ http/rest_client.go
@@ -0,0 +1,28 @@
+package http
+
+import "github.com/go-resty/resty/v2"
+
+var (
+	NewRestClient  = resty.New
+	NewRestRequest = NewRestClient().R
+)
+
+func NewRestJsonRequest(token ...string) (r *resty.Request) {
+	r = NewRestRequest()
+	// POST Struct, default is JSON content type. No need to set one
+	r.SetHeader("Content-Type", "application/json")
+	r.SetHeader("Accept", "application/json")
+	if len(token) > 0 {
+		r.SetAuthToken(token[0])
+	}
+	return
+}
+
+func NewRestFormRequest(token ...string) (r *resty.Request) {
+	r = NewRestRequest()
+	r.SetHeader("Content-Type", "application/x-www-form-urlencoded")
+	if len(token) > 0 {
+		r.SetAuthToken(token[0])
+	}
+	return
+}
