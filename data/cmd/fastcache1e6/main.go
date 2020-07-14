26875120f56cd6c94dabd9685dc4388797f38c82
Angenal <angenal@hotmail.com>
2020-07-14 12:06:11 +0800

    Update main.go

--- data/cmd/fastcache1e6/main.go
+++ data/cmd/fastcache1e6/main.go
@@ -4,18 +4,20 @@
 // go build -ldflags "-s -w" -o A:/test/ .
 
 // TEST: fastcache1e6 -c 2 -d 128 -t 10000000
-// cache1.benchmark(4CPU+16G+MHD) GET:20Mq/s SET:2Mq/s FLUSH:0.4s
-// cache2.benchmark(8CPU+16G+SSD) GET:20Mq/s SET:2Mq/s FLUSH:0.1s
-// buntdb-benchmark(8CPU+16G+SSD) GET:5Mq/s  SET:230Kq/s
+// cache1(4CPU+16G+MHD).benchmark GET:20Mq/s SET:2Mq/s FLUSH:0.4s
+// cache2(8CPU+16G+SSD).benchmark GET:20Mq/s SET:2Mq/s FLUSH:0.1s
+// buntdb(8CPU+16G+SSD).benchmark GET:5Mq/s  SET:230Kq/s
 
 // SHM: fastcache1e6 -prod=true -s=0 -d=0 -a=ipc://cache0
 
 // TCP: fastcache1e6 -prod=true -s=2 -p=6060
-// CSharp.benchmark(4CPU+16G+MHD) GET:60Kq/s SET:60Kq/s
+// CSharp(4CPU+16G+MHD).benchmark GET:60Kq/s SET:60Kq/s
 
 // WS: fastcache1e6 -prod=true -s=3 -p=6060
 
 // NatS: fastcache1e6 -prod=true -s=4 -p=4222 -name=cache -token=HGJ766GR767FKJU0
+// CSharp(4CPU+16G+MHD).benchmark -type SUITE -count 100000 -size 8 -token HGJ766GR767FKJU0
+// CSharp(4CPU+16G+MHD).benchmark PUB:200K~2Mq/s PUBSUB:150K~1Mq/s REQREP:5K~7Kq/s
 
 package main
 
