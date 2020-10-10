5f44973fba16366beffdf6fa068aee7668f9aa2b
Angenal <angenal@hotmail.com>
2020-10-10 09:38:37 +0800

    Create convert_encoding.go

---
+++ f/convert_encoding.go
@@ -0,0 +1,18 @@
+package f
+
+import (
+	"bytes"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+)
+
+// GbkToUtf8 convert encoding-simplifiedchinese.
+func GbkToUtf8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
