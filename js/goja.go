2fdada71a96fedeeadbbfb993d0ff9e790965341
yangzhou <angenal@hotmail.com>
2020-11-01 18:28:12 +0800

    update

--- js/goja.go
+++ js/goja.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// Console console.log in javascript.
 func Console(r *goja.Runtime) {
 	console := r.NewObject()
 
@@ -22,11 +23,19 @@ func Console(r *goja.Runtime) {
 	r.Set("console", console)
 }
 
+// Db execute sql in javascript.
+// 	db.q: return ResultObject
+// 	db.q('select * from table1 where id=?',1)
+// 	db.q('select * from table1 where id=:id',{id:1})
+// 	db.i: return LastInsertId
+// 	db.i('insert into table1 values(?,?)',1,'test')
+// 	db.i('insert into table1 values(:id,:name)',{id:1,name:'test'})
+//  db.x: return RowsAffected
+//  db.x('update table1 set name=? where id=?','test',1)
+//  db.x('update table1 set name=:name where id=:id',{id:1,name:'test'})
 func Db(r *goja.Runtime, d *sqlx.DB) {
 	db := r.NewObject()
 
-	// db.q('select * from table1 where id=?',1)
-	// db.q('select * from table1 where id=:id',{id:1})
 	_ = db.Set("q", func(c goja.FunctionCall) goja.Value {
 		v, l := goja.Null(), len(c.Arguments)
 		if l == 0 {
@@ -66,8 +75,95 @@ func Db(r *goja.Runtime, d *sqlx.DB) {
 			if err = rows.MapScan(result); err != nil {
 				return r.ToValue(err)
 			}
+			for k, v := range result {
+				if s, ok := v.([]byte); ok {
+					result[k] = string(s)
+				}
+			}
 			v = r.ToValue(result)
 		}
+
+		return v
+	})
+
+	_ = db.Set("i", func(c goja.FunctionCall) goja.Value {
+		v, l := r.ToValue(-1), len(c.Arguments)
+		if l == 0 {
+			return v
+		}
+
+		var (
+			sql      = c.Arguments[0].String()
+			insertId int64
+			value    map[string]interface{}
+			hasValue bool
+		)
+
+		if l == 2 {
+			value, hasValue = c.Arguments[1].Export().(map[string]interface{})
+		}
+
+		if hasValue {
+			if rows, err := d.Exec(sql, value); err != nil {
+				return r.ToValue(err)
+			} else {
+				insertId, _ = rows.LastInsertId()
+			}
+		} else {
+			values := make([]interface{}, 0, l-1)
+			if l > 1 {
+				for _, a := range c.Arguments[1:] {
+					values = append(values, a.Export())
+				}
+			}
+			if rows, err := d.Exec(sql, values...); err != nil {
+				return r.ToValue(err)
+			} else {
+				insertId, _ = rows.LastInsertId()
+			}
+		}
+		v = r.ToValue(insertId)
+
+		return v
+	})
+
+	_ = db.Set("x", func(c goja.FunctionCall) goja.Value {
+		v, l := r.ToValue(-1), len(c.Arguments)
+		if l == 0 {
+			return v
+		}
+
+		var (
+			sql      = c.Arguments[0].String()
+			affected int64
+			value    map[string]interface{}
+			hasValue bool
+		)
+
+		if l == 2 {
+			value, hasValue = c.Arguments[1].Export().(map[string]interface{})
+		}
+
+		if hasValue {
+			if rows, err := d.Exec(sql, value); err != nil {
+				return r.ToValue(err)
+			} else {
+				affected, _ = rows.RowsAffected()
+			}
+		} else {
+			values := make([]interface{}, 0, l-1)
+			if l > 1 {
+				for _, a := range c.Arguments[1:] {
+					values = append(values, a.Export())
+				}
+			}
+			if rows, err := d.Exec(sql, values...); err != nil {
+				return r.ToValue(err)
+			} else {
+				affected, _ = rows.RowsAffected()
+			}
+		}
+		v = r.ToValue(affected)
 
 		return v
 	})
--- js/goja_test.go
+++ js/goja_test.go
@@ -3,6 +3,8 @@ package js
 import (
 	"testing"
 
+	"github.com/angenalZZZ/gofunc/f"
+
 	"github.com/angenalZZZ/gofunc/configfile"
 	"github.com/dop251/goja"
 	"github.com/jmoiron/sqlx"
@@ -36,29 +38,36 @@ func TestDb(t *testing.T) {
 	if !ok {
 		t.SkipNow()
 	}
+
 	var d *sqlx.DB
 	if d, err = sqlx.Connect("mysql", conn["mysql"].(string)); err != nil {
 		t.Fatal(err)
 	}
 	defer func() { _ = d.Close() }()
-	var rows *sqlx.Rows
-	sql, values := "SELECT * FROM logtest ORDER BY Id DESC LIMIT ?", []interface{}{1}
-	if rows, err = d.Queryx(sql, values...); err != nil {
-		t.Fatal(err)
-	}
-	for rows.Next() {
-		result := make(map[string]interface{})
-		if err = rows.MapScan(result); err != nil {
-			t.Fatal(err)
-		}
-		t.Logf("Max(Id): %v", result["Id"])
-	}
 
 	Db(r, d)
-	script := `console.log('Max(Id): '+db.q('SELECT * FROM logtest ORDER BY Id DESC LIMIT ?',1).Id)`
-	if v, err := r.RunString(script); err != nil {
+
+	var newId int64
+	script := `db.i('insert into logtest(Code,Type,Message,Account,CreateTime) values(?,?,?,?,?)','test',2,'new message','admin','2020-10-10 10:20:30')`
+	if res, err := r.RunString(script); err != nil {
 		t.Fatal(err)
-	} else if !v.Equals(goja.Undefined()) {
-		t.Fail()
+	} else {
+		newId = res.ToInteger()
+		t.Logf("inserted rows Id: %d", newId)
+	}
+
+	script = `console.log('update rows affected: '+db.x('update logtest set Code=? where Id=?','200',` + f.ToString(newId) + "))"
+	if _, err := r.RunString(script); err != nil {
+		t.Fatal(err)
+	}
+
+	script = `console.log('select rows: '+JSON.stringify(db.q('select * from logtest where Id=?',` + f.ToString(newId) + ")))"
+	if _, err := r.RunString(script); err != nil {
+		t.Fatal(err)
+	}
+
+	script = `console.log('delete rows affected: '+db.x('delete from logtest where Id=?',` + f.ToString(newId) + "))"
+	if _, err := r.RunString(script); err != nil {
+		t.Fatal(err)
 	}
 }
