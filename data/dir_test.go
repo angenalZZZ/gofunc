946fb98ccb84f243da16f68206c77e4b10dc096b
yangzhou <angenal@hotmail.com>
2020-11-10 16:53:45 +0800

    update

--- data/dir_test.go
+++ data/dir_test.go
@@ -1,6 +1,8 @@
 package data
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/angenalZZZ/gofunc/f"
@@ -11,4 +13,15 @@ func TestDir(t *testing.T) {
 	t.Log(CurrentPath)
 	t.Log(CurrentDir, CurrentFile)
 	t.Log(CurrentUserName, CurrentUserHomeDir, f.PathExists(CurrentUserHomeDir))
+
+	path := filepath.Join(CurrentDir, ".nats")
+	if exists, isDir := f.FileExist(path); !exists || !isDir {
+		if exists {
+			_ = os.RemoveAll(path)
+		}
+		if err := os.Mkdir(path, 0644); err != nil {
+			t.Fatal(err)
+		}
+	}
+	t.Log(path, f.PathExists(path), f.IsDir(path))
 }
