0b871a09a0b4c7ac05f61e1611a257ab304db037
yangzhou <angenal@hotmail.com>
2020-11-17 20:08:31 +0800

    update

--- cmd/natsql/test-/001/natsql.js
+++ cmd/natsql/test-/001/natsql.js
@@ -1,4 +1,4 @@
-//通用扩展
+//扩展方法
 Date.prototype.Add = function (seconds) { var t = new Date(); t.setTime(this.getTime() + seconds * 1000); return t; };
 Date.prototype.AddDate = Date.prototype.AddDays = function (days) { var t = new Date(); t.setTime(this.getTime() + days * 24 * 3600 * 1000); return t; };
 Date.prototype.Date = function () { return this.toISOString().split("T")[0]; };
@@ -7,21 +7,23 @@ Date.prototype.DateTime = function () { return this.toISOString().replace("T", "
 String.prototype.Date = function () { return this.replace("T", " ").split(" ")[0]; };
 String.prototype.Time = function () { return this.replace("T", " ").split(" ")[1].split(".")[0]; };
 String.prototype.DateTime = function () { return this.replace("T", " ").split(".")[0]; };
+function col(s) { if (!s) { return "NULL"; } return "'" + s.replace("'", "''") + "'"; };
 
 //订阅处理
 function sql(records) {
-    if (!records || records.constructor.name != "Array") return "";
+    if (!records || records.constructor.name != "Array") return;
     var items = records.filter(function (item) { return item.constructor.name == "Object" && item.hasOwnProperty("Code"); });
-    if (items.length == 0) return "";
-    var sqlString = "insert into logtest(Code,Type,Message,Account,CreateTime) values"
+    if (items.length == 0) return;
+    var sqlString = "insert into logtest(Type,Code,Message,Account,CreateTime,CreateUser) values"
         + items.map(function (item) {
             return "("
-                + "'" + item.Code.replace("'", "") + "',"
                 + item.Type + ","
-                + "'" + item.Message.replace("'", "''") + "',"
-                + "'" + item.Account + "',"
-                + "'" + item.CreateTime.replace("T", " ").split(".")[0] + "'"
-                + ")";
+                + col(item.Code) + ","
+                + col(item.Message) + ","
+                + col(item.Account) + ","
+                + col(item.CreateTime.DateTime()) + ","
+                + col(item.CreateUser) + ")";
         }).join(",") + ";";
-    console.log(sqlString);
+    // console.log(sqlString);
+    return sqlString;
 }
